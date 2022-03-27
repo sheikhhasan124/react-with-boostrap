26e14e92d9ea4e14f2eb7d6402635f4185a140dc
hasan-ullah <mdhasanullah124@gmail.com>
2022-03-28 05:03:45 +0600

    card group add

--- src/Components/CardGroup/CardGroup.js
+++ src/Components/CardGroup/CardGroup.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import Card from '../Card/Card';
 
 const CardGroup = () => {
+  const datas = [
+    {
+      id:1,
+      title:'hero',
+    },
+    {
+      id:2,
+      title:'bero',
+    },
+    {
+      id:3,
+      title:'dero',
+    }
+  ]
     return (
         <div>
             <h2>hello fprm card g</h2>
+            <div className="card-group">
+             {datas.map(data=><Card key={data.id} data={data}/>)}
+</div>
         </div>
     );
 };
